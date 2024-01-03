667c5c9cbb7a085eb4ad0d0da60b4fd4b1cde76d
didedoshka <didedoshka@gmail.com>
2024-01-03 23:30:06 +0300

    implement simple palette

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,5 +1,7 @@
 #version 330 core
 
+#define MAX_ITER 1000
+
 in vec2 coords;
 out vec4 color;
 
@@ -27,19 +29,25 @@ vec2 fractal_func(vec2 z, vec2 c) {
     return complex_add(complex_mult(z, z), c);
 }
 
+vec3 color_by_iter(int iter) {
+    float proportion = (1.0 * iter) / MAX_ITER;
+    vec3 base_color = vec3(1, 0.55, 0);
+    return base_color * (1 - proportion);
+}
+
 void main() {
     vec2 z = vec2(0, 0);
     vec2 camera_coords = (coords * 0.5 + vec2(0.5, 0.5)) * camera_width + camera_corner;
     // vec2 camera_coords = vec2((coords.x * 0.5 + 0.5) * camera_width, coords.y * 0.5 + 0.5);
     // vec2 camera_coords = coords;
     
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < MAX_ITER; ++i) {
         z = fractal_func(z, camera_coords);
+        if (complex_squared_abs(z) >= 4) {
+            color = vec4(color_by_iter(i + 1), 1);
+            return;
+        }
     }
-    if (complex_squared_abs(z) < 4) {
-        color = vec4(1, 1, 1, 1);
-    } else {
-        color = vec4(0, 0, 0, 1);
-    }
+    color = vec4(color_by_iter(MAX_ITER), 1);
     // color = vec4(coords, 0.0f, 1.0f);
 }
